6b22e6e875d30da30ad2c82ab4ff0e4cd68da7b2
Jimmy Shong <jimmysh341@gmail.com>
2021-10-09 15:49:11 -0400

    Added Point Class
    Created Point Class with getters, setters, and str.

--- tckfilereader/Point.py
+++ tckfilereader/Point.py
@@ -1,5 +1,30 @@
-
-class Point:
-    def __init__(self, x:float, y:float, z:float, t:float) -> None:
+class Point(object):
+    def __init__(self, x: float, y: float, z: float, t: float) -> None:
         """Creates a point with the specified x, y, z, and t coordinates"""
+        self.x = x
+        self.y = y
+        self.z = z
+        self.t = t
+
+    def __iter__(self):
         pass
+
+    def __str__(self) -> str:
+        """Displays Point Object"""
+        return "Point(%s,%s,%s,%s)" % (self.x, self.y, self.z, self.t)
+
+    def get_x(self) -> float:
+        """Defines x variables"""
+        return self.x
+
+    def get_y(self) -> float:
+        """Defines y variables"""
+        return self.y
+
+    def get_z(self) -> float:
+        """Defines z variables"""
+        return self.z
+
+    def get_t(self) -> float:
+        """Defines t variables"""
+        return self.t
