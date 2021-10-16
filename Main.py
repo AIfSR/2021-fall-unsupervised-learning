2d62778d55413c71efc892386bffa754448d351e
SPD3 <sean@ayerdoyles.org>
2021-10-16 15:12:21 -0400

    Added a main file to go through all the feature creation.

---
+++ Main.py
@@ -0,0 +1,23 @@
+from plotting.SimpleComparePlots import TwoDComparePlots
+from features.XFeatureCreator import XFeatureCreator
+from features.YFeatureCreator import YFeatureCreator
+from features.ZFeatureCreator import ZFeatureCreator
+from features.TFeatureCreator import TFeatureCreator
+from features.RateOfChangeFeatureCreator import RateOfChangeFeatureCreator
+from features.PointsDistanceFeatureCreator import PointsDistanceFeatureCreator
+
+if __name__ == "__main__":
+    plotFeatures = [
+        (XFeatureCreator(), TFeatureCreator()),
+        (YFeatureCreator(), TFeatureCreator()),
+        (ZFeatureCreator(), TFeatureCreator()),
+        (RateOfChangeFeatureCreator(XFeatureCreator()), TFeatureCreator()),
+        (RateOfChangeFeatureCreator(YFeatureCreator()), TFeatureCreator()),
+        (RateOfChangeFeatureCreator(ZFeatureCreator()), TFeatureCreator()),
+        (RateOfChangeFeatureCreator(RateOfChangeFeatureCreator(XFeatureCreator())), TFeatureCreator()),
+        (RateOfChangeFeatureCreator(RateOfChangeFeatureCreator(YFeatureCreator())), TFeatureCreator()),
+        (RateOfChangeFeatureCreator(RateOfChangeFeatureCreator(ZFeatureCreator())), TFeatureCreator()),
+        (PointsDistanceFeatureCreator(), TFeatureCreator())
+        
+    ]
+    simpleComparePlots = TwoDComparePlots()
