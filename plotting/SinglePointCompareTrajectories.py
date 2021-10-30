d49e51964494fbc65e2f05c260679ffd5576302b
SPD3 <sean@ayerdoyles.org>
2021-10-30 12:23:43 -0400

    Allowed for custom X values on the singlepointscompare trajectories.

--- plotting/SinglePointCompareTrajectories.py
+++ plotting/SinglePointCompareTrajectories.py
@@ -17,22 +17,27 @@ class SinglePointCompareTrajectories (ComparePlotsBase):
             count += 1
         return sum / count
 
-    def display_plots(self, featureCreator:FeatureCreatorBase, categories:List[Tuple[str,List[Points]]]) -> None:
+    def display_plots(self, features:Tuple[Features, Features], categories:List[Tuple[str,List[Points]]]) -> None:
         """Displays a plot comparing the average of a feature for each category"""
         for name, allFilesPoints in categories:
-            pointsAverages = []
+            xPointsAverages = []
+            yPointsAverage = []
             for trajectoryPoints in allFilesPoints:
-                feature  = featureCreator.get_features(trajectoryPoints)
-                avgOfFeature = self._get_average_of_feature(feature)
-                pointsAverages.append(avgOfFeature)
-            plt.scatter(range(len(pointsAverages)), pointsAverages, label=name)
+                xfeature = features[0].get_features(trajectoryPoints)
+                yfeature = features[1].get_features(trajectoryPoints)
+                xAvgOfFeature = self._get_average_of_feature(xfeature)
+                yAvgOfFeature = self._get_average_of_feature(yfeature)
+                xPointsAverages.append(xAvgOfFeature)
+                yPointsAverage.append(yAvgOfFeature)
+            plt.scatter(xPointsAverages, yPointsAverage, label=name)
         
         title = ""
         for name, allFilesPoints in categories:
             title += name + ", "
-        title += str(featureCreator) + " comparison"
+        title += str(features[0]) + str(features[1]) + " comparison"
         plt.title(title)
-        plt.ylabel(str(featureCreator))
+        plt.xlabel(str(features[0]))
+        plt.ylabel(str(features[1]))
         plt.legend()
         plt.show()
         
