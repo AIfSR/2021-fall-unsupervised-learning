d36f3ac0683c293dc01d7784961db5c36d33742b
leodilla <20218256+leodilla@users.noreply.github.com>
2021-10-30 15:46:03 -0400

    Add files via upload

--- features/ZFeatureCreator.py
+++ features/ZFeatureCreator.py
@@ -2,17 +2,17 @@ from features.FeatureCreatorBase import FeatureCreatorBase
 from features.Features import Features
 from tckfilereader.Points import Points
 
-class XFeatureCreator (FeatureCreatorBase):
-    """Creates a Feature that is just the X coordinates of the points"""
+class ZFeatureCreator (FeatureCreatorBase):
+    """Creates a Feature that is just the Z coordinates of the points"""
 
     def get_features(self, points:Points) -> Features:
-        """Gets all the X values as features"""
+        """Gets all the Z values as features"""
         features = Features()
         for point in points:
-            features.add_feature_val(point.get_x())
+            features.add_feature_val(point.get_z())
 
         return features
 
     def __str__(self) -> str:
-        """This is a feature for X coords"""
-        return "X"
+        """This is a feature for Z coords"""
+        return "Z"
