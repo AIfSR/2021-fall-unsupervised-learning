12cdbb23b52348e4b830f89eed66dafe32bd2fef
Jiminator <jimmysh341@gmail.com>
2021-11-06 18:23:24 -0400

    Finished adding histogram code

--- plotting/SinglePointCompareTrajectories.py
+++ plotting/SinglePointCompareTrajectories.py
@@ -57,19 +57,38 @@ class SinglePointCompareTrajectories (ComparePlotsBase):
                     bottom=False,      # ticks along the bottom edge are off
                     top=False,         # ticks along the top edge are off
                     labelbottom=False)
-            left, width = 0.1, 0.65
-            bottom, height = 0.1, 0.65
+            left, width = 0.05, 0.6
+            bottom, height = 0.05, 0.6
             spacing = 0.005
             rect_scatter = [left, bottom, width, height]
-            rect_histx = [left, bottom + height + spacing, width, 0.2]
-            rect_histy = [left + width + spacing, bottom, 0.2, height]
+
+            # rect_histx = [left, bottom + height + spacing, width, 0.095]
+            rect_hist_m0_x = [left, bottom + height + spacing, width, 0.095]
+            rect_hist_m1_x = [left, bottom + height + spacing + spacing + 0.095, width, 0.095]
+            rect_hist_m2_x = [left, bottom + height + spacing + spacing + 0.095 + spacing + 0.095, width, 0.095]
+
+            # rect_histy = [left + width + spacing, bottom, 0.095, height]
+            rect_hist_m0_y = [left + width + spacing, bottom, 0.095, height]
+            rect_hist_m1_y = [left + width + spacing + spacing + 0.095, bottom, 0.095, height]
+            rect_hist_m2_y = [left + width + spacing + spacing + 0.095 + spacing + 0.095 , bottom, 0.095, height]
+
             fig = plt.figure()
             ax_scatter = plt.axes(rect_scatter)
             ax_scatter.tick_params(direction='in', top=True, right=True)
-            ax_histx = plt.axes(rect_histx)
-            ax_histx.tick_params(direction='in', labelbottom=False)
-            ax_histy = plt.axes(rect_histy)
-            ax_histy.tick_params(direction='in', labelleft=False)
+
+            ax_hist_m0_x = plt.axes(rect_hist_m0_x)
+            ax_hist_m0_x.tick_params(direction='in', labelbottom=False)
+            ax_hist_m1_x = plt.axes(rect_hist_m1_x)
+            ax_hist_m1_x.tick_params(direction='in', labelbottom=False)
+            ax_hist_m2_x = plt.axes(rect_hist_m2_x)
+            ax_hist_m2_x.tick_params(direction='in', labelbottom=False)
+
+            ax_hist_m0_y = plt.axes(rect_hist_m0_y)
+            ax_hist_m0_y.tick_params(direction='in', labelleft=False)
+            ax_hist_m1_y = plt.axes(rect_hist_m1_y)
+            ax_hist_m1_y.tick_params(direction='in', labelleft=False)
+            ax_hist_m2_y = plt.axes(rect_hist_m2_y)
+            ax_hist_m2_y.tick_params(direction='in', labelleft=False)
                   
             for name, allFilesPoints in categories:
                 if(type(xFeatureGenerator) == LineFeatureCreator):
@@ -77,6 +96,12 @@ class SinglePointCompareTrajectories (ComparePlotsBase):
                 categoryCount += 1
                 xPointsAverages = []
                 yPointsAverages = []
+                m0_xPointsAverages = []
+                m0_yPointsAverages = []
+                m1_xPointsAverages = []
+                m1_yPointsAverages = []
+                m2_xPointsAverages = []
+                m2_yPointsAverages = []
                 for trajectoryPoints in allFilesPoints:
                     xFeature = xFeatureGenerator.get_features(trajectoryPoints)
                     yFeature = yFeatureGenerator.get_features(trajectoryPoints)
@@ -84,6 +109,8 @@ class SinglePointCompareTrajectories (ComparePlotsBase):
                     yAvgOfFeature = self._get_average_of_feature(yFeature)
                     xPointsAverages.append(xAvgOfFeature)
                     yPointsAverages.append(yAvgOfFeature)
+
+
             
                 # plt.scatter(xPointsAverages, yPointsAverages, label=name)
                 ax_scatter.scatter(xPointsAverages, yPointsAverages, label=name)
@@ -94,13 +121,38 @@ class SinglePointCompareTrajectories (ComparePlotsBase):
                 # ax_scatter.set_ylim((-lim, lim))
 
                 # bins = np.arange(-lim, lim + binwidth, binwidth)
-                ax_histx.hist(xPointsAverages)
-                ax_histy.hist(yPointsAverages, orientation='horizontal')
-                ax_histx.set_ylabel("Number of Trajectories", fontsize = 8)
-                ax_histy.set_xlabel("Number of Trajectories", fontsize = 8)
+                if name == "M0":
+                    ax_hist_m0_x.hist(xPointsAverages)
+                    ax_hist_m0_y.hist(yPointsAverages, orientation='horizontal')
+                if name == "M1":
+                    ax_hist_m1_x.hist(xPointsAverages, color="orange")
+                    ax_hist_m1_y.hist(yPointsAverages, orientation='horizontal' , color="orange")
+                if name == "M2":
+                    ax_hist_m2_x.hist(xPointsAverages, color="green")
+                    ax_hist_m2_y.hist(yPointsAverages, orientation='horizontal', color="green")
+
+                ax_hist_m0_x.set_ylabel("Trajectories", fontsize = 8)
+                ax_hist_m0_y.set_xlabel("Trajectories", fontsize = 8)
+                ax_hist_m1_x.set_ylabel("Trajectories", fontsize = 8)
+                ax_hist_m1_y.set_xlabel("Trajectories", fontsize = 8)
+                ax_hist_m2_x.set_ylabel("Trajectories", fontsize = 8)
+                ax_hist_m2_y.set_xlabel("Trajectories", fontsize = 8)
 
                 # ax_histx.set_xlim(ax_scatter.get_xlim())
                 # ax_histy.set_ylim(ax_scatter.get_ylim())
+                # ax_hist_m0_x.set_xlim(ax_scatter.get_xlim())
+                ax_hist_m0_x.set_ylim([0,8])
+                ax_hist_m1_x.set_ylim([0,8])
+                ax_hist_m2_x.set_ylim([0,8])
+                ax_hist_m0_y.set_xlim([0,8])
+                ax_hist_m1_y.set_xlim([0,8])
+                ax_hist_m2_y.set_xlim([0,8])
+                ax_hist_m0_x.set_xlim(ax_scatter.get_xlim())
+                ax_hist_m0_y.set_ylim(ax_scatter.get_ylim())
+                ax_hist_m1_x.set_xlim(ax_scatter.get_xlim())
+                ax_hist_m1_y.set_ylim(ax_scatter.get_ylim())
+                ax_hist_m2_x.set_xlim(ax_scatter.get_xlim())
+                ax_hist_m2_y.set_ylim(ax_scatter.get_ylim())
 
             title = ""
             for name, allFilesPoints in categories:
@@ -108,9 +160,11 @@ class SinglePointCompareTrajectories (ComparePlotsBase):
             if not type(xFeatureGenerator) == LineFeatureCreator:
                 # plt.xlabel(xLabel)
                 ax_scatter.set_xlabel(xLabel)
-                title += str(yFeatureGenerator) + " vs. " + str(xFeatureGenerator) + " comparison"
+                # title += str(yFeatureGenerator) + " vs. " + str(xFeatureGenerator) + " comparison"
+                title += str(yLabel) + " vs. " + str(xLabel) + " comparison"
             else:
-                title += str(yFeatureGenerator)
+                # title += str(yFeatureGenerator)
+                title += str(yLabel)
 
             # plt.ylabel(yLabel)
             ax_scatter.set_ylabel(yLabel)
