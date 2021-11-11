51a2eeb326885ae475614353072ebb7fce7d0e95
SPD3 <sean@ayerdoyles.org>
2021-11-11 10:44:33 -0500

    Should have been with last commit. Just got rid of the linefeaturecreator code from the singlepointcomparetrajectories.

--- plotting/singlepointcomparetrajectories/SinglePointCompareTrajectories.py
+++ plotting/singlepointcomparetrajectories/SinglePointCompareTrajectories.py
@@ -6,6 +6,7 @@ from features.Features import Features
 from features.XFeatureCreator import XFeatureCreator
 from plotting.ComparePlotsBase import ComparePlotsBase
 from tckfilereader.Points import Points
+from plotting.singlepointcomparetrajectories.LineFeatureCreator import LineFeatureCreator
 
 class SinglePointCompareTrajectories (ComparePlotsBase):
 
@@ -172,21 +173,4 @@ class SinglePointCompareTrajectories (ComparePlotsBase):
             fig.legend()
             plt.show()
 
-class LineFeatureCreator (FeatureCreatorBase):
 
-    def __init__(self) -> None:
-        super().__init__()
-        self._val = 0.0
-
-    def increment(self):
-        self._val += 1
-
-    def get_features(self, points:Points) -> Features:
-        """Creates a feature with only 0s so the scatter plot is a strait line"""
-        feature = Features()
-        for point in points:
-            feature.add_feature_val(self._val)
-        return feature
-
-    def __str__(self) -> str:
-        return ""
